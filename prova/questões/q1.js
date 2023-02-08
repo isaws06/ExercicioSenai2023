3108feaa7047d637d70f47b6a754168a50fd78bb
Isadora Wenzel da Silveira <99267300+isaws06@users.noreply.github.com>
2023-02-08 09:48:26 -0300

    Delete prova/questões directory

--- prova/questões/q1.js
+++
@@ -1,9 +0,0 @@
-var a, b, c, d, media 
-var a = 10 
-var b = 20 
-var c = a * b 
-var d = a + b + c
-media = d/4
-
-console.log("Questão 1:" + "A- " + a, "B- " + b, "C- " + c, "D- " + d, "Média- " + media);
-
