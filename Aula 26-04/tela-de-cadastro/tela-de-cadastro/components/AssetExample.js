30778af732f8b719f8ce3a5d21f71d8b927c42c0
Isadora Wenzel da Silveira <99267300+isaws06@users.noreply.github.com>
2023-04-26 09:21:33 -0300

    Add files via upload

---
+++ Aula 26-04/tela-de-cadastro/tela-de-cadastro/components/AssetExample.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, } from 'react-native';
+
+export default function SignupScreen() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  function handleSignUp() {
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    
+    // Add your code to sign up the user here
+  }
+
+  return (
+    <View style={styles.container}>
+       <Image source={require('../assets/face.jpg')} style={styles.logo} />
+      <Text style={styles.title}>Cadastre-se</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
+      />
+
+        <TextInput
+        style={styles.input}
+        placeholder="Confirmar senha"
+        onChangeText={setConfirmPassword}
+        value={confirmPassword}
+        secureTextEntry={true}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+        <Text style={styles.buttonText}>Cadastrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+
+  logo: {
+    height: 100,
+    width: 250,
+    marginBottom: 20,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 32,
+  },
+
+  input: {
+    width: '80%',
+    height: 40,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginBottom: 17,
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    padding: 12,
+    borderRadius: 8,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
