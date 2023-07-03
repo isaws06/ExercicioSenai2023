a0c951cf6a64230646f3d2d92fe83b3d4188c26c
Isadora Wenzel da Silveira <99267300+isaws06@users.noreply.github.com>
2023-07-03 12:54:53 -0300

    Add files via upload

---
+++ Aula 03-07/script.js
@@ -0,0 +1,24 @@
+function busca(){    
+    let cod = document.getElementById("cod").value
+    let url = "./ceps.json"
+    let cidade = document.getElementById("cidade")
+    let bairro = document.getElementById("bairro")
+    let estado = document.getElementById("estado")
+    let pais = document.getElementById("pais")
+    
+
+    fetch(url)
+    .then( res => res.json())
+    .then(data => {
+        
+        for(var cep of data.ceps){
+            if (cod == cep.codigo) {
+                cidade.value = cep.cidade
+                bairro.value = cep.bairro
+                estado.value = cep.estado
+                pais.value = cep.pais
+                break
+            }
+        }
+    })
+}
